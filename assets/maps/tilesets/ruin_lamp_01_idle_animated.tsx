ed0e273f1bcd8edb363367a54ddae006bb3519e9
kiana <amanikiana6@mail.com>
2026-07-12 05:37:11 +0330

    added animation

---
+++ assets/maps/tilesets/ruin_lamp_01_idle_animated.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="ruin_lamp_01_idle_animated" tilewidth="206" tileheight="310" tilecount="6" columns="6">
+ <image source="ruin_lamp_01_idle_sheet.png" width="1236" height="310"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="140"/>
+   <frame tileid="1" duration="140"/>
+   <frame tileid="2" duration="140"/>
+   <frame tileid="3" duration="140"/>
+   <frame tileid="4" duration="140"/>
+   <frame tileid="5" duration="140"/>
+  </animation>
+ </tile>
+</tileset>
